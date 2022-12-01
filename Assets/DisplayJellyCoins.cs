d5bb86649cf6d48b9bdc296cb4f480a96371c1c1
HappyGamer3194 <happygamer3194@gmail.com>
2022-12-01 13:26:45 -0800

    Added a display text in main menu.

---
+++ Assets/DisplayJellyCoins.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayJellyCoins : MonoBehaviour
+{
+    public Text display;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        display.text = PlayerPrefs.GetInt("totalJellyCoins", 0).ToString();
+    }
+
+    public void UpdateCoins()
+    {
+        display.text = PlayerPrefs.GetInt("totalJellyCoins", 0).ToString();
+    }
+}
